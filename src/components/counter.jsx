9718c0a0ae087cea96da18ae0db6962cbdd0c20c
Christina <cphanth@gmail.com>
2020-10-25 00:11:45 -0400

    created a react component

---
+++ src/components/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() { 
+        return <h1>Hello World</h1>;    //this is a JSX expression, NOT returning a string
+    }
+}
+ 
+export default Counter;
