e6708cc678e8dbcde7ca8ce4b726badc6cf14405
Christina <cphanth@gmail.com>
2020-10-25 13:43:48 -0400

    added Increment button using React.Fragment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>;    //this is a JSX expression, NOT returning a string
-    }
+  render() {
+    return (
+      //div is the container for the react application (but we don't want multiple divs)
+      //so use React.fragment
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+      /* this is a JSX expression, NOT returning a string
+      JSX unable to render 2 elements h1 and button together
+      so must wrap in a div element
+      */
+    );
+  }
 }
- 
+
 export default Counter;
