9f7d310f9e451545c80af3fdbba42d4f698bab40
Christina <cphanth@gmail.com>
2020-10-25 15:01:28 -0400

    remove image element and URL

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,21 +3,22 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
   }; //state is an object that has properties or data that the component needs
   render() {
     return (
       <React.Fragment>
-        <img src={this.state.imageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
       /* this is a JSX expression, NOT returning a string
       JSX unable to render 2 elements h1 and button together
       so must wrap in a div element.
+
       div is the container for the react application (but we don't want multiple divs)
       so use React.Fragment.
+
       You can write any expression within the <span> curly brackets when choosing not to hardcode
+      
       Set image attribute dynamically
       Curly braces render values dynamically
       */
