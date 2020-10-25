fc33eeb4d4eb22fcbdbf5ce7d452a1f6b5985611
Christina <cphanth@gmail.com>
2020-10-25 00:00:24 -0400

    imported bootstrap to improve modern look

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'       //bootstrap helps our app look modern and sleek
 
 ReactDOM.render(
   <React.StrictMode>
