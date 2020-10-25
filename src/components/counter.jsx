212ccaa0e9d3e3984a6a7ecb6a53f707042c54b3
Christina <cphanth@gmail.com>
2020-10-25 14:42:53 -0400

    embedding expression in span

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 1,
+  }; //state is an object that has properties or data that the component needs
   render() {
     return (
-      //div is the container for the react application (but we don't want multiple divs)
-      //so use React.fragment
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount}</span>
         <button>Increment</button>
       </React.Fragment>
       /* this is a JSX expression, NOT returning a string
       JSX unable to render 2 elements h1 and button together
-      so must wrap in a div element
+      so must wrap in a div element.
+      div is the container for the react application (but we don't want multiple divs)
+      so use React.Fragment.
+      You can write any expression within the <span> curly brackets when choosing not to hardcode
       */
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
