fe01956911d659f9c7c3213e53c0f4789b7d345a
Christina <cphanth@gmail.com>
2020-10-27 21:10:26 -0400

    create method to handle product event argument

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -28,9 +28,15 @@ class Counter extends Component {
   //  this.handleIncrement = this.handleIncrement.bind(this);
   //}
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
     console.log("Increment Clicked", this);
     this.setState({ count: this.state.count + 1 });
+    console.log(product);
+  };
+
+  //temporarily define method to handle passing an event argument
+  doHandleInrement = () => {
+    this.handleIncrement({ id: 1 });
   };
 
   render() {
@@ -38,7 +44,7 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={this.doHandleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
